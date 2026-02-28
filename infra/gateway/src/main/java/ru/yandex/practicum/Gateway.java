cde43b574cc21102733fc4bce7f9eaa9aaffd621
Evgeniya <eva.sharapova1@mail.ru>
2026-02-28 13:38:44 +0300

    add infra

--- infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
+++ infra/gateway/src/main/java/ru/yandex/practicum/Gateway.java
@@ -1,18 +1,14 @@
-package stats.server;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan({
-        "stats.server",
-        "dto"
-})
-public class StatsServerApplication {
+public class Gateway {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 }
