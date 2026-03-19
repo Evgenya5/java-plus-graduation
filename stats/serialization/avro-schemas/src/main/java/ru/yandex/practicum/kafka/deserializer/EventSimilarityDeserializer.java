f2e01519c3dcc4036ad3899048f74bc9598ac9a3
Evgeniya <eva.sharapova1@mail.ru>
2026-03-19 22:11:20 +0300

    add serialization

---
+++ stats/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/deserializer/EventSimilarityDeserializer.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.kafka.deserializer;
+
+import ru.yandex.practicum.ewm.stats.avro.EventSimilarityAvro;
+
+public class EventSimilarityDeserializer extends BaseAvroDeserializer<EventSimilarityAvro> {
+    public EventSimilarityDeserializer() {
+        super(EventSimilarityAvro.getClassSchema());
+    }
+}
